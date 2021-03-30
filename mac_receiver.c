fa6fd6a201c36ee4eae6dbe5dbe0a7bd1b201e4f
Steve Anchise <steve.anchise@gmail.com>
2021-03-30 14:05:30 +0200

    token receiver test ready

--- mac_receiver.c
+++ mac_receiver.c
@@ -11,13 +11,55 @@
 #include <string.h>
 #include "main.h"
 
+/*
+void MAC_DATA_REQUEST(station, SAPI, data)
+{
+	
+}
+*/
 
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacReceiver(void *argument)
 {
-
+	struct queueMsg_t queueMsg;
+	uint8_t * msg;
+	uint8_t * qPtr;
+	osStatus_t retCode;
+	size_t	size;
+	
+	for(;;) {
+		//wait for data, check queue
+		retCode = osMessageQueueGet(queue_macR_id, &queueMsg, NULL, osWaitForever);
+		CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
+		qPtr = queueMsg.anyPtr;
+		
+		
+		//read message
+		if (qPtr[1] == TOKEN_TAG)    						// is it a token frame ?
+		{
+		  size = TOKENSIZE;											// yes -> token frame size
+			
+			//todo after token OK, move after the "end if"
+			msg = osMemoryPoolAlloc(memPool,osWaitForever);
+			memcpy(msg,&qPtr[1],size-2);
+			queueMsg.anyPtr = msg;
+			queueMsg.type = TOKEN;
+			retCode = osMessageQueuePut(
+				queue_macS_id,
+				&queueMsg,
+				osPriorityNormal,
+				osWaitForever);
+			CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
+		}
+		else
+		{
+			//do nothing yet
+			//size = qPtr[3]+6;    									// size of string + 6 (ETX,...)
+		}
+				
+	}	
 }
 
 
